490d17ea357b389ca04804ecb83614d0339769d6
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-07-06 03:13:33 +0530

    add listview in trending fragment

--- app/src/main/java/com/example/signin/HomePage.java
+++ app/src/main/java/com/example/signin/HomePage.java
@@ -1,14 +1,69 @@
 package com.example.signin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
+
+    private TextView actionBerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+
+        actionBerName=findViewById(R.id.action_ber_name);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new TagsFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+
+                        case R.id.nav_tags:
+                            selectedFragment = new TagsFragment();
+                            actionBerName.setText("#tags");
+                            break;
+                        case R.id.nav_trending:
+                            selectedFragment = new TrendingFragment();
+                            actionBerName.setText("Trending now");
+                            break;
+                        case R.id.nav_explore:
+                            selectedFragment = new ExploreFragment();
+                            actionBerName.setText("Explore #tags");
+                            break;
+                        case R.id.nav_saved:
+                            selectedFragment = new SavedFragment();
+                            actionBerName.setText("Saved #tags");
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
